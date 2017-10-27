d97195d72737284130d4f4d08b79e40fa33e1362
Ross Waycaster <rosswaycaster@me.com>
2017-10-27 17:29:20 -0500

    initial commit

---
+++ handler.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.helloWorld = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+      thanks2webpack: 'es2017!'.padStart(16, 'oh look, ') //
+    }),
+  };
+
+  callback(null, response);
+};
